53e05eb1ab06828edd443bb61acabf25d9bb3901
Avashcodex <89736333+Avashcodex@users.noreply.github.com>
2023-02-08 01:09:22 +0530

    levl3

---
+++ Assets/Scripts/LevelCalScripts/Level_3.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_3 : MonoBehaviour
+{
+    private char num1 = 'a';
+    private char num2 = 'a';
+    private char symb = 'a';
+    public GameObject emptyBox1;
+    public GameObject emptyBox2;
+    public GameObject emptyBox3;
+    private void Update()
+    {
+        checker();
+    }
+
+    void checker()
+    {
+        num1 = emptyBox1.GetComponent<SymbolIdentifier>().Symbol;
+        num2 = emptyBox3.GetComponent<SymbolIdentifier>().Symbol;
+        symb = emptyBox2.GetComponent<SymbolIdentifier>().Symbol;
+
+        if (num1 != 'a' && num2 != 'a')
+        {
+            int a = num1 - '0';
+            int b = num2 - '0';
+            if (a - b == 7)
+            {
+                Debug.Log("Winner");
+            }
+            else
+            {
+                Debug.Log(a + b);
+            }
+        }
+
+    }
+}
