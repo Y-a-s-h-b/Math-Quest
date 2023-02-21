283e4ecc7385f5525278e9ca7d86e7ec4c7bd7bf
Y-a-s-h-b <yash.bansal2021@vitbhopal.ac.in>
2023-02-22 01:16:51 +0530

    Jump and End Screen

--- Assets/UI Scripts/Movement.cs
+++ Assets/UI Scripts/Movement.cs
@@ -17,27 +17,31 @@ public class Movement : MonoBehaviour
     public Transform Witch1Trns;
     public Transform Witch2Trns;
     public Transform Witch3Trns;
+    private bool isJumping = false;
     
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim= GetComponent<Animator>();
+        anim = GetComponent<Animator>();
+        
         inputModule = GetComponent<StandaloneInputModule>();
         anim.enabled = true;
         if (LevelLoad.intSaver == 1)
         {
             this.transform.position = Witch1Trns.position;
         }
-        else if (LevelLoad.intSaver ==2)
+        else if (LevelLoad.intSaver == 2)
         {
             this.transform.position = Witch2Trns.position;
         }
-        else if (LevelLoad.intSaver ==3)
+        else if (LevelLoad.intSaver == 3)
         {
             this.transform.position = Witch3Trns.position;
         }
-        
+
     }
 
     // Update is called once per frame
@@ -45,55 +49,68 @@ public class Movement : MonoBehaviour
     {
         movX = joystick.Horizontal * speed;
         float verticalMove = joystick.Vertical;
-            rb.velocity = new Vector2(movX*speed,rb.velocity.y );
-            if (joystick.Horizontal >= 0.5f)
-            {
+        rb.velocity = new Vector2(movX * speed, rb.velocity.y);
+        if (joystick.Horizontal >= 0.5f)
+        {
             transform.eulerAngles = new Vector3(0, 180, 0);
-            
-                //anim.SetTrigger("Run");
+
+            //anim.SetTrigger("Run");
             anim.SetBool("idle", false);
             anim.SetBool("walk", false);
             anim.SetBool("running", true);
         }
-            else if (joystick.Horizontal <= -0.5f )
-            {
+        else if (joystick.Horizontal <= -0.5f)
+        {
             transform.eulerAngles = new Vector3(0, 0, 0);
             movX = -speed;
-                //anim.SetTrigger("Run");
+            //anim.SetTrigger("Run");
             anim.SetBool("idle", false);
             anim.SetBool("walk", false);
             anim.SetBool("running", true);
         }
 
-            else if(joystick.Horizontal > 0f && joystick.Horizontal < 0.5f) 
+        else if (joystick.Horizontal > 0f && joystick.Horizontal < 0.5f)
         {
             transform.eulerAngles = new Vector3(0, 180, 0);
             anim.SetBool("idle", false);
             anim.SetBool("running", false);
             anim.SetBool("walk", true);
-            
+
         }
-            else if(joystick.Horizontal > -0.5f && joystick.Horizontal < 0f)
+        else if (joystick.Horizontal > -0.5f && joystick.Horizontal < 0f)
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
             anim.SetBool("idle", false);
             anim.SetBool("running", false);
             anim.SetBool("walk", true);
         }
-            else
-            {
+        else
+        {
             //anim.SetTrigger("Idle");
             anim.SetBool("idle", true);
             anim.SetBool("running", false);
             anim.SetBool("walk", false);
             movX = 0;
-            }
-        /*if (joystick.Horizontal < 0.2f && joystick.Horizontal > -0.2f && joystick.Vertical < 0.5f)
+        }
+        if (joystick.Vertical > 0.5f && !isJumping)
         {
-            anim.Play("Idle_01");
-        }*/
+            //rb.AddForce(new Vector2(rb.velocity.x, jump));
+            anim.SetTrigger("jump");
+            anim.Play("Jump");
+            rb.AddForce(Vector3.up * jump, ForceMode2D.Impulse);
+            
+            isJumping = true;
 
+        }
+        if(joystick.Vertical < 0.5f)
+        {
+            isJumping = false;
+            
+
+        }
     }
+    
+    
     private void OnTriggerEnter2D(Collider2D Collider)
     {
         if (Collider.gameObject.tag == "NPC")
