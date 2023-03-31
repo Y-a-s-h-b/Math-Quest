d5cdb269c980faa0fd26afb81b02b73cbe0befaf
Y-a-s-h-b <yash.bansal2021@vitbhopal.ac.in>
2023-03-31 10:57:02 +0530

    Added New Scene

--- Assets/UI Scripts/DialogueTrigger.cs
+++ Assets/UI Scripts/DialogueTrigger.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().startDialogue(dialogue);
